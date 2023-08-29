56f69a924657ab3fd003c7544781077894309b15
Ayush Verma <142798351+vermaayush0@users.noreply.github.com>
2023-08-29 01:07:34 -0700

    code to find Hypotenuse of a Triangle

---
+++ Q9_ayush_verma.py
@@ -0,0 +1,8 @@
+h = eval(input("Enter the value of Height : "))
+b = eval(input("Enter the value Base : "))
+
+h = (h**2)+(b**2)
+H1 = h**0.5
+
+print("Hypotenuse of a Triangle is : ",H1)
+#ayush Verma
